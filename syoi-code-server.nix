050db40c2f0856e20f55c79c34c7f6762a04bc88
Tommy Li <dev@stdx.space>
2024-12-26 03:29:40 +0800

    object storage configuration for forgejo

--- syoi-code-server.nix
+++ syoi-code-server.nix
@@ -71,13 +71,25 @@
     database = {
       type = "sqlite3";
     };
+    secrets = {
+      storage = {
+        MINIO_ENDPOINT = config.sops.secrets.r2-endpoint.path;
+        MINIO_ACCESS_KEY_ID = config.sops.secrets.r2-access-key.path;
+        MINIO_SECRET_ACCESS_KEY = config.sops.secrets.r2-secret-key.path;
+      };
+    };
     settings = {
       server = {
         DOMAIN = "git.syoi.org";
-        ROOT_URL = "https://${config.services.forgejo.settings.server.DOMAIN}/"; 
+        ROOT_URL = "https://${config.services.forgejo.settings.server.DOMAIN}/";
       };
       service = {
         DISABLE_REGISTRATION = true;
+      };
+      storage = {
+        STORAGE_TYPE = "minio";
+        MINIO_BUCKET = "forgejo-data";
+        MINIO_USE_SSL = true;
       };
     };
   };
@@ -96,6 +108,30 @@
         owner = "code";
         group = "code";
         restartUnits = [ "cloudflared-tunnel-code-server.service" ];
+      };
+      r2-access-key = {
+        sopsFile = ./secrets/r2.json;
+        key = "access_key";
+        format = "json";
+        owner = "forgejo";
+        group = "forgejo";
+        restartUnits = [ "forgejo.service" ];
+      };
+      r2-secret-key = {
+        sopsFile = ./secrets/r2.json;
+        key = "secret_key";
+        format = "json";
+        owner = "forgejo";
+        group = "forgejo";
+        restartUnits = [ "forgejo.service" ];
+      };
+      r2-endpoint = {
+        sopsFile = ./secrets/r2.json;
+        key = "endpoint";
+        format = "json";
+        owner = "forgejo";
+        group = "forgejo";
+        restartUnits = [ "forgejo.service" ];
       };
     };
   };
